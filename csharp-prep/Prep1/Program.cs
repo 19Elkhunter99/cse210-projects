52f361613681f963123b71bee2cc8610a8b79c1a
19Elkhunter99 <woodconnor100@gmail.com>
2025-04-26 14:36:39 -0600

    Prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,18 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Prep1 World!");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your first name?");
+            string first=Console.ReadLine();
+
+            Console.WriteLine("What is your last name?");
+            string last=Console.ReadLine();
+            Console.WriteLine($"Your name is {last}, {first} {last}.");
+            
+
+
+
+        }
     }
-}
